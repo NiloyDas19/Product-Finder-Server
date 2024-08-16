a2f7754c4a738b4ec0a9f0ea3b2ed1e3fc8b7183
NiloyDas19 <niloydas5215@gmail.com>
2024-08-16 10:24:22 +0600

    connect to dadabase

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         
+        const productsCollection = client.db("productsDB").collection('products');
+
+        
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
