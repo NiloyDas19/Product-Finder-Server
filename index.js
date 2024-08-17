f17045ef0f57d757c2fa49a83a30ecf98a865f8e
NiloyDas19 <niloydas5215@gmail.com>
2024-08-17 07:57:45 +0600

    fiexd some bug

--- index.js
+++ index.js
@@ -108,9 +108,7 @@ async function run() {
                 res.status(500).json({ message: error.message });
             }
         });
-
-
-
+        
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
